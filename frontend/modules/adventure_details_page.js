5c9569ead93a28238aa05daf2355eeb9abd7119a
Deepak Tiwari <ainutiwari483@gmail.com>
2023-04-03 23:26:35 +0530

    completed bootstrap carousel

--- frontend/modules/adventure_details_page.js
+++ frontend/modules/adventure_details_page.js
@@ -55,7 +55,92 @@ function addAdventureDetailsToDOM(adventure) {
 function addBootstrapPhotoGallery(images) {
   // TODO: MODULE_ADVENTURE_DETAILS
   // 1. Add the bootstrap carousel to show the Adventure images
+
+  //Getting the HTML Elements to insert to the photo gallery
+  const photoGalleryEl = document.getElementById("photo-gallery");
+
+  //Clear the photo gallery content
+  photoGalleryEl.innerHTML = "";
+
+  //Create coursel components
+  const carousel = document.createElement("div");
+  carousel.classList.add("carousel", "slide");
+  carousel.setAttribute("id", "carouselExampleIndicators");
+  carousel.setAttribute("data-bs-ride", "carousel");
+
+  const indicators = document.createElement("div");
+  indicators.classList.add("carousel-indicators");
+  carousel.appendChild(indicators);
+
+  const inner = document.createElement("div");
+  inner.classList.add("carousel-inner");
+  carousel.appendChild(inner);
+
+  const prevButton = document.createElement("button");
+  prevButton.classList.add("carousel-control-prev");
+  prevButton.setAttribute("type", "button");
+  prevButton.setAttribute("data-bs-target", "#carouselExampleIndicators");
+  prevButton.setAttribute("data-bs-slide", "prev");
+  carousel.appendChild(prevButton);
+
+  const prevIcon = document.createElement("span");
+  prevIcon.classList.add("carousel-control-prev-icon");
+  prevIcon.setAttribute("aria-hidden", "true");
+  prevButton.appendChild(prevIcon);
+
+  const prevLabel = document.createElement("span");
+  prevLabel.classList.add("visually-hidden");
+  prevLabel.textContent = "Previous";
+  prevButton.appendChild(prevLabel);
+
+  const nextButton = document.createElement("button");
+  nextButton.classList.add("carousel-control-next");
+  nextButton.setAttribute("type", "button");
+  nextButton.setAttribute("data-bs-target", "#carouselExampleIndicators");
+  nextButton.setAttribute("data-bs-slide", "next");
+  carousel.appendChild(nextButton);
+
+  const nextIcon = document.createElement("span");
+  nextIcon.classList.add("carousel-control-next-icon");
+  nextIcon.setAttribute("aria-hidden", "true");
+  nextButton.appendChild(nextIcon);
+
+  const nextLabel = document.createElement("span");
+  nextLabel.classList.add("visually-hidden");
+  nextLabel.textContent = "Next";
+  nextButton.appendChild(nextLabel);
+
+  images.forEach((image, index) => {
+    const indicator = document.createElement("button");
+    indicator.setAttribute("type", "button");
+    indicator.setAttribute("data-bs-target", "#carouselExampleIndicators");
+    indicator.setAttribute("data-bs-slide-to", index.toString());
+
+    if (index === 0) {
+      indicator.classList.add("active");
+      inner.appendChild(createCarouselItem(image, true));
+    } else {
+      inner.appendChild(createCarouselItem(image, false));
+    }
+    indicators.appendChild(indicator);
+  });
+  photoGalleryEl.appendChild(carousel);
+
+  function createCarouselItem(imageSrc, active) {
+    const item = document.createElement("div");
+    item.classList.add("carousel-item");
+    if (active) {
+      item.classList.add("active");
+    }
+    const image = document.createElement("img");
+    image.classList.add("d-block", "w-100");
+    image.setAttribute("src", imageSrc);
+    item.appendChild(image);
+
+    return item;
+  }
 }
+
 
 //Implementation of conditional rendering of DOM based on availability
 function conditionalRenderingOfReservationPanel(adventure) {
