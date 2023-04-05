1b3614deecfc5b4a9ac546600a5fabbded2342ef
Deepak Tiwari <ainutiwari483@gmail.com>
2023-04-05 15:10:24 +0530

    function showbanner is remaining

--- frontend/modules/adventure_details_page.js
+++ frontend/modules/adventure_details_page.js
@@ -128,12 +128,13 @@ function addBootstrapPhotoGallery(images) {
 
   function createCarouselItem(imageSrc, active) {
     const item = document.createElement("div");
-    item.classList.add("carousel-item");
+    item.classList.add("carousel-item", "h-100");
     if (active) {
       item.classList.add("active");
     }
     const image = document.createElement("img");
-    image.classList.add("d-block", "w-100");
+    image.classList.add("d-block", "w-100", "h-100");
+    image.setAttribute("style", "object-fit:cover");
     image.setAttribute("src", imageSrc);
     item.appendChild(image);
 
@@ -141,18 +142,37 @@ function addBootstrapPhotoGallery(images) {
   }
 }
 
-
 //Implementation of conditional rendering of DOM based on availability
 function conditionalRenderingOfReservationPanel(adventure) {
   // TODO: MODULE_RESERVATIONS
   // 1. If the adventure is already reserved, display the sold-out message.
-  if(adventure)
+  if (adventure["available"]) {
+    const soldOutPanel = document.getElementById("reservation-panel-sold-out");
+    const panelAvailable = document.getElementById(
+      "reservation-panel-available"
+    );
+    panelAvailable.style.display = "block";
+    soldOutPanel.style.display = "none";
+
+    const personPerCost = document.getElementById("reservation-person-cost");
+    personPerCost.textContent = adventure.costPerHead;
+  } else {
+    const panelAvailable = document.getElementById(
+      "reservation-panel-available"
+    );
+    const soldOutPanel = document.getElementById("reservation-panel-sold-out");
+    panelAvailable.style.display = "none";
+    soldOutPanel.style.display = "block";
+  }
 }
 
 //Implementation of reservation cost calculation based on persons
 function calculateReservationCostAndUpdateDOM(adventure, persons) {
   // TODO: MODULE_RESERVATIONS
   // 1. Calculate the cost based on number of persons and update the reservation-cost field
+  const reservationCost = document.getElementById("reservation-cost");
+  const reservationPerCost = document.getElementById("reservation-person-cost");
+  return (reservationCost.innerHTML = persons * adventure.costPerHead);
 }
 
 //Implementation of reservation form submission
@@ -160,6 +180,39 @@ function captureFormSubmit(adventure) {
   // TODO: MODULE_RESERVATIONS
   // 1. Capture the query details and make a POST API call using fetch() to make the reservation
   // 2. If the reservation is successful, show an alert with "Success!" and refresh the page. If the reservation fails, just show an alert with "Failed!".
+  console.log(adventure);
+  const myForm = document.getElementById("myForm");
+  myForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = {
+      name: myForm.elements["name"].value,
+      date: myForm.elements["date"].value,
+      person: myForm.elements["person"].value,
+      adventure: adventure.id,
+    };
+    console.log(formData);
+    try {
+      const response = await fetch(
+        `${config.backendEndpoint}/reservations/new`,
+        {
+          method: "POST",
+          body: JSON.stringify(formData),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        alert("Success");
+        window.location.reload();
+      } else {
+        alert("Failed");
+      }
+    } catch (error) {
+      alert("Failed");
+      console.log(error);
+    }
+  });
 }
 
 //Implementation of success banner after reservation
